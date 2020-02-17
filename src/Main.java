f848ddf56645da0c3bbff0de919e84b06ea90fff
vclimpont <46527505+vclimpont@users.noreply.github.com>
2020-02-17 16:15:41 +0100

    init project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEEEEEEEEEEEEEEEEEEEEEEEEEY SALUT A TOUS LES AMIS C'EST DAVID LAFARGE POKEMON");
+	}
+
+}
