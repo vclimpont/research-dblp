b0153ec23fbd36e9aee52ddd1f98d4cc26128388
vclimpont <46527505+vclimpont@users.noreply.github.com>
2020-02-17 16:33:06 +0100

    import graphstream

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("");
+		
+		Graph g = new SingleGraph("test");
 	}
 
 }
