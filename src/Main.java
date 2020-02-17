04b4ac6e283e161804a9216b4aad7be5200e394a
valentinLampriere <valentin.lampriere1@gmail.com>
2020-02-17 16:19:34 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HEEEEEEEEEEEEEEEEEEEEEEEEEY SALUT A TOUS LES AMIS C'EST DAVID LAFARGE POKEMON");
+		System.out.println("");
 	}
 
 }
